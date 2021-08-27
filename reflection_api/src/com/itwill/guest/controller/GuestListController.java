b6455ad6b3e4baa245a79c8721d25f0f17bfad47
narae <iampnr91@gmail.com>
2021-08-27 14:23:16 +0900

    ..

---
+++ reflection_api/src/com/itwill/guest/controller/GuestListController.java
@@ -0,0 +1,11 @@
+package com.itwill.guest.controller;
+
+import java.util.ArrayList;
+
+public class GuestListController {
+	public String handleRequest() {
+		String forwardPath="forward:/WEB-INF/view/guest_list.jsp";
+		
+		return forwardPath;
+	}
+}

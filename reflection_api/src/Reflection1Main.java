8e7595a74098e7b5cb80ba7a16e0b57b8506cb30
narae <iampnr91@gmail.com>
2021-08-27 14:49:42 +0900

    ..

--- reflection_api/src/Reflection1Main.java
+++ reflection_api/src/Reflection1Main.java
@@ -7,19 +7,25 @@ public class Reflection1Main {
 		 * 생성자 직접 호출없이(GuestListController 클래스타입을 지칭하지않고) GuestListController객체 생성
 		 */
 		
-		
 		/*
 		 * 1. com.itwill.guest.controller.GuestListController.class 클래스를 메모리위로 로딩
 		 */
-		String className = "com.itwill.guest.controller.GuestListController";
-		Class anyClass = Class.forName(className);
+		String className1 = "com.itwill.guest.controller.GuestListController";
+		String className2 = "com.itwill.guest.controller.GuestMainController";
+		String className3 = "com.itwill.guest.controller.GuestViewController";
+		Class anyClass1 = Class.forName(className1);
+		Class anyClass2 = Class.forName(className2);
+		Class anyClass3 = Class.forName(className3);
 		
 		/*
 		 * 2. 로딩된 클래스의 기본생성자 호출
 		 */
-		Object object = anyClass.newInstance();
-		
-		System.out.println(object);
+		Object object1 = anyClass1.newInstance();
+		Object object2 = anyClass2.newInstance();
+		Object object3 = anyClass3.newInstance();
+		System.out.println(object1);
+		System.out.println(object2);
+		System.out.println(object3);
 		
 		
 	}

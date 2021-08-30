c1e6ca7b2861e2ed696a8c93e75cbdd14ecb0851
narae <iampnr91@gmail.com>
2021-08-30 16:30:09 +0900

    ..

--- user_model2/src/main/java/com/itwill/user/controller/UserWriteActionController.java
+++ user_model2/src/main/java/com/itwill/user/controller/UserWriteActionController.java
@@ -24,29 +24,29 @@ public class UserWriteActionController implements Controller{
 		
 		if(request.getMethod().equalsIgnoreCase("GET")){
 			forwardPath="redirect:user_write_form.do";
-		} else {
-			try {
-				request.setCharacterEncoding("UTF-8");
-				String userId=request.getParameter("userId");
-				String password=request.getParameter("password");
-				String name=request.getParameter("name");
-				String email=request.getParameter("email");
-				User newUser=new User(userId,password,name,email);
-				//UserService userService=new UserService();
-				int result = this.userService.create(newUser);
-				if(result==-1){
-					//아이디중복
-					String msg=URLEncoder.encode(userId+" 는 이미존재하는 아이디입니다.", "UTF-8");
-					forwardPath="forward:user_write_form.do?msg="+msg;
-				}else if(result==1){
-					//회원가입성공
-					forwardPath="redirect:user_login_form.do";
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				forwardPath="redirect:user_error.do";
+			return forwardPath;
+		} 
+		
+		//request.setCharacterEncoding("UTF-8");
+		String userId=request.getParameter("userId");
+		String password=request.getParameter("password");
+		String name=request.getParameter("name");
+		String email=request.getParameter("email");
+		User newUser=new User(userId,password,name,email);
+		//UserService userService=new UserService();
+		try {	
+			int result = this.userService.create(newUser);
+			if(result==-1){ //아이디중복
+				String msg=URLEncoder.encode(userId+" 는 이미존재하는 아이디입니다.", "UTF-8");
+				forwardPath="redirect:user_write_form.do?msg="+msg;
+			}else if(result==1){ //회원가입성공
+				forwardPath="redirect:user_login_form.do";
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			forwardPath="redirect:user_error.do";
 		}
+		
 		//return "forward:/WEB-INF/views/user_write_action.jsp";
 		return forwardPath;
 	}

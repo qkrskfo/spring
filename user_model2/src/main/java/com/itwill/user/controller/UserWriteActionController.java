5f5884a29bf8b41fde132dc8afca984a947424e6
narae <iampnr91@gmail.com>
2021-08-30 13:34:00 +0900

    ..

---
+++ user_model2/src/main/java/com/itwill/user/controller/UserWriteActionController.java
@@ -0,0 +1,16 @@
+package com.itwill.user.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itwill.summer.Controller;
+
+public class UserWriteActionController implements Controller{
+
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		return "forward:/WEB-INF/views/user_write_action.jsp";
+	}
+	
+	
+}

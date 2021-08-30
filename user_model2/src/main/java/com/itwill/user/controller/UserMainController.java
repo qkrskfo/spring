0c9a625e5fc5ec5cbbe8de7e09a0a9481b89bd9f
narae <iampnr91@gmail.com>
2021-08-30 13:34:26 +0900

    ..

--- user_model2/src/main/java/com/itwill/user/controller/UserMainController.java
+++ user_model2/src/main/java/com/itwill/user/controller/UserMainController.java
@@ -9,8 +9,10 @@ public class UserMainController implements Controller {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		
 		String forwardPath="forward:/WEB-INF/views/user_main.jsp";
 		return forwardPath;
+		
 	}
 	
 }
--- user_model2/src/main/java/com/itwill/user/controller/UserWriteActionController.java
+++ user_model2/src/main/java/com/itwill/user/controller/UserWriteActionController.java
@@ -9,7 +9,9 @@ public class UserWriteActionController implements Controller{
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		
 		return "forward:/WEB-INF/views/user_write_action.jsp";
+		
 	}
 	
 	

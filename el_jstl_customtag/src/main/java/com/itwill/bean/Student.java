299fcdcd1c4bf216a95d29cd6f426f2bf385b39d
narae <iampnr91@gmail.com>
2021-08-31 13:59:45 +0900

    ..

--- el_jstl_customtag/src/main/java/com/itwill/bean/Student.java
+++ el_jstl_customtag/src/main/java/com/itwill/bean/Student.java
@@ -2,13 +2,26 @@ package com.itwill.bean;
 
 public class Student {
 
-	private int no; //property
-	private String name; //property
+	private int no; //property type --> int
+	private String name; //property type --> String
+	private Car car; //property type --> Car
 	
 	public Student() {
 		// TODO Auto-generated constructor stub
 	}
 
+	//getter
+	public Car getCar() {
+		return this.car;
+	}
+	
+	//setter
+	public void setCar(Car car) {
+		this.car = car;
+	}
+	
+	
+	
 	public int getNo() {
 		return no;
 	}

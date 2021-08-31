08d6dc9c255586b456b11195a40e7dd204ec4ebf
narae <iampnr91@gmail.com>
2021-08-31 13:24:30 +0900

    ..

---
+++ reflection_api/src/Reflection2Main.java
@@ -0,0 +1,15 @@
+import com.itwill.guest.controller.GuestListController;
+
+public class Reflection2Main {
+
+	public static void main(String[] args) throws Exception{
+		
+		
+		String className="com.itwill.guest.Guest";
+		
+		
+		//Class anyClass1 = Class.forName(className1); 
+		
+	}
+
+}

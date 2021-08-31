beb0a513bc4b569f139ffc44dfb6fdf93f123e08
narae <iampnr91@gmail.com>
2021-08-31 13:54:33 +0900

    ..

---
+++ el_jstl_customtag/src/main/java/com/itwill/bean/Student.java
@@ -0,0 +1,29 @@
+package com.itwill.bean;
+
+public class Student {
+
+	private int no; //property
+	private String name; //property
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}

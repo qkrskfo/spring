8780df1d6962eeeb6070b80b29192f14c7309696
narae <iampnr91@gmail.com>
2021-09-02 15:22:44 +0900

    ..

--- user_model2/src/main/java/com/itwill/filter/LoginCheckFilter.java
+++ user_model2/src/main/java/com/itwill/filter/LoginCheckFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LoginCheckFilter implements Filter{
 
@@ -15,6 +17,16 @@ public class LoginCheckFilter implements Filter{
 			throws IOException, ServletException {
 		// 모든 요청을 다 받으면 안됨. userMain같은건 로그인 체크를 할 필요가 없잖앙
 		
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		
+		String sUserId = (String)req.getSession().getAttribute("sUserId");
+		if(sUserId==null) {
+			resp.sendRedirect("user_main.do");
+			return;
+		}
+		
+		chain.doFilter(request, response);
 		
 	}
 	

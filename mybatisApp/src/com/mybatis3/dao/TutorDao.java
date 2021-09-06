0246dea7ebd4061bab70f3a7bbc73f49de964392
narae <iampnr91@gmail.com>
2021-09-06 15:52:00 +0900

    ..

--- mybatisApp/src/com/mybatis3/dao/TutorDao.java
+++ mybatisApp/src/com/mybatis3/dao/TutorDao.java
@@ -24,10 +24,25 @@ public class TutorDao
 			e.printStackTrace();
 		}
 	}
-	
-	public Tutor findTutorByIdWithCoursesAndAddress(Integer tutorId) {
 		
-		return null;
+	/*******************************************
+	 	SELECT TUTOR + COURSES JOIN [ 1 : N ]
+	 *******************************************/
+	public Tutor findTutorByIdWithCourses(Integer tutorId) {
+		/*
+		<< TutorMapper.xml --> namespace : com.mybatis3.dao.mapper.TutorMapper >>
+		<select id="findTutorByIdWithCourses" parameterType="java.lang.Integer"
+											  resultMap="tutorWithCoursesResultMap">
+			SELECT t.tutor_id, t.name as tutor_name, email,course_id, c.name, description, start_date, end_date
+		    FROM tutors t 
+		    left outer join courses c 
+			on t.tutor_id=c.tutor_id
+			where t.tutor_id=#{tutorId}
+		</select>
+		 */
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		Tutor tutor = sqlSession.selectOne(NAMESPACE+"findTutorByIdWithCoursesAndAddress");
+		return tutor;
 	}
 }
 

56683097f878bee5406420f32d0d53a3a6b7da02
narae <iampnr91@gmail.com>
2021-09-06 14:22:26 +0900

    ..

--- mybatisApp/src/com/mybatis3/dao/AddressDao.java
+++ mybatisApp/src/com/mybatis3/dao/AddressDao.java
@@ -3,8 +3,11 @@ package com.mybatis3.dao;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.mybatis3.domain.Address;
 
 public class AddressDao {
 	private SqlSessionFactory sqlSessionFactory;
@@ -17,4 +20,15 @@ public class AddressDao {
 			e.printStackTrace();
 		}
 	}
+	
+	/****************************  
+	SELECT [addresses + students JOIN ] (1 : N) 
+	*****************************/
+	public Address findAddressByIdWithStudents(Integer addrId) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		Address address = sqlSession.selectOne("com.mybatis3.dao.mapper.AddressMapper.findAddressByIdWithStudents", addrId);
+		sqlSession.close();
+		return address;
+	}
+	
 }

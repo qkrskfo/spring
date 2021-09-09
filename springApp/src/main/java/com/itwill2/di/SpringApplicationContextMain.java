4d0b6916afc38ede0304663615c2a6f808444d06
narae <iampnr91@gmail.com>
2021-09-09 16:17:26 +0900

    ..

--- springApp/src/main/java/com/itwill2/di/SpringApplicationContextMain.java
+++ springApp/src/main/java/com/itwill2/di/SpringApplicationContextMain.java
@@ -14,7 +14,14 @@ public class SpringApplicationContextMain {
 		System.out.println("------------Spring Container초기화시작----------------");
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/itwill2/di/2.bean_dependency_injection.xml");
 		System.out.println("------------Spring Container초기화끝------------------");
+		
+		System.out.println("### 1. Setter Injection(setter 메소드를 통한 의존성 주입)");
 		UserService userService=(UserService)applicationContext.getBean("userService");
+		userService.findUser(null);
+		userService.findUserList();
+		
+		System.out.println("### 2. Constructor Injection(생성자를 통한 의존성 주입)");
+		UserService conUserService=(UserService)applicationContext.getBean("conUserService");
 		userService.findUser(null);
 		userService.findUserList();
 		

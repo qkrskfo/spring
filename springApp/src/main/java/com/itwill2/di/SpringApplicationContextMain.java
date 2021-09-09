5038e26c6058971989338b8bc39f06ade3f5795b
narae <iampnr91@gmail.com>
2021-09-09 15:17:46 +0900

    ..

--- springApp/src/main/java/com/itwill2/di/SpringApplicationContextMain.java
+++ springApp/src/main/java/com/itwill2/di/SpringApplicationContextMain.java
@@ -1,4 +1,4 @@
-package com.itwill1.bean.create;
+package com.itwill2.di;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 

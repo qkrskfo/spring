4900a892579b4aa65e699cbd792b0b0899af0662
narae <iampnr91@gmail.com>
2021-09-09 14:33:38 +0900

    ..

---
+++ springApp/src/main/java/com/itwill1/bean/create/PrototypeBean.java
@@ -0,0 +1,9 @@
+package com.itwill1.bean.create;
+
+public class PrototypeBean {
+	
+	public PrototypeBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- springApp/src/main/java/com/itwill1/bean/create/SpringApplicationContextMain.java
+++ springApp/src/main/java/com/itwill1/bean/create/SpringApplicationContextMain.java
@@ -12,6 +12,21 @@ public class SpringApplicationContextMain {
 		System.out.println("------------Spring Container초기화시작----------------");
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/itwill1/bean/create/1.bean_create.xml");
 		System.out.println("------------Spring Container초기화끝------------------");
+		
+		System.out.println("------- bean create -------");
+		CreateBean1 createBean1 = (CreateBean1)applicationContext.getBean("createBean1");
+		System.out.println("### "+createBean1);
+		CreateBean2 createBean2 = (CreateBean2)applicationContext.getBean(CreateBean2.class);
+		System.out.println("### "+createBean2);
+		
+		System.out.println("------- scope[singleton] -------");
+		SingletonBean singletonBean1 = (SingletonBean)applicationContext.getBean("singletonBean");
+		SingletonBean singletonBean2 = (SingletonBean)applicationContext.getBean("singletonBean");
+		SingletonBean singletonBean3 = (SingletonBean)applicationContext.getBean("singletonBean");
+		
+		System.out.println(singletonBean1);
+		System.out.println(singletonBean2);
+		System.out.println(singletonBean3);
 
 	}
 

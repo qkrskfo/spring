7c84d767468547eaf509adabcaaabd1b0bd6e9f4
narae <iampnr91@gmail.com>
2021-09-10 16:09:19 +0900

    ..

--- springApp/src/main/java/com/itwill3/dao/user/SpringUserDaoImplJDBCTestMain.java
+++ springApp/src/main/java/com/itwill3/dao/user/SpringUserDaoImplJDBCTestMain.java
@@ -1,11 +1,19 @@
 package com.itwill3.dao.user;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringUserDaoImplJDBCTestMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 	
-	
-
+	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/itwill3/dao/user/3.user_dao_jdbc.xml");
+	UserDao userDao = (UserDao)applicationContext.getBean("userDao");
+	/*
+	userDao.create(new User(null, null, null, null));
+	userDao.findUser(null);
+	userDao.findUserList();
+	 */
 	}
 
 }

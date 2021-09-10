38ec6e84056298c6bcf5ba65e7fbd78a39f329a2
narae <iampnr91@gmail.com>
2021-09-10 15:38:14 +0900

    ..

--- user_model2_el_jstl_mybatis/src/main/java/com/itwill/user/UserDaoImplMyBatis.java
+++ user_model2_el_jstl_mybatis/src/main/java/com/itwill/user/UserDaoImplMyBatis.java
@@ -1,43 +1,63 @@
 package com.itwill.user;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class UserDaoImplMyBatis implements UserDao {
-
+	
+	public static final String NAMESPACE="com.itwill.user.mapper.UserMapper.";
+	private SqlSessionFactory sqlSessionFactory;
+	
+	public UserDaoImplMyBatis() {
+		try {
+			InputStream myBatisConfigInputStream = Resources.getResourceAsStream("mybatis-config.xml");
+			this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(myBatisConfigInputStream);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public int create(User user) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return sqlSessionFactory.openSession(true).insert(NAMESPACE+"create", user);
 	}
 
 	@Override
 	public int update(User user) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return sqlSessionFactory.openSession(true).update(NAMESPACE+"update", user);
 	}
 
 	@Override
 	public int remove(String userId) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return sqlSessionFactory.openSession(true).delete(NAMESPACE+"delete", userId);
 	}
 
 	@Override
 	public User findUser(String userId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return sqlSessionFactory.openSession(true).selectOne(NAMESPACE+"findUser", userId);
 	}
 
 	@Override
 	public ArrayList<User> findUserList() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> tempUserList =sqlSessionFactory.openSession(true).selectList(NAMESPACE+"findUserList");
+		ArrayList<User> userList = (ArrayList<User>)tempUserList;
+		return userList;
+		
 	}
 
 	@Override
 	public boolean existedUser(String userId) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		int count = sqlSessionFactory.openSession(true).selectOne(NAMESPACE+"existedUser", userId);
+		if(count==1) {
+			return true;			
+		} else {
+			return false;
+		}
 	}
 
 }

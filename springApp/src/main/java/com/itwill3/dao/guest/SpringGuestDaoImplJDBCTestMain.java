b43ee75378b2348686bc8218a27371c1458bee06
narae <iampnr91@gmail.com>
2021-09-10 11:52:35 +0900

    ..

--- springApp/src/main/java/com/itwill3/dao/guest/SpringGuestDaoImplJDBCTestMain.java
+++ springApp/src/main/java/com/itwill3/dao/guest/SpringGuestDaoImplJDBCTestMain.java
@@ -5,10 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringGuestDaoImplJDBCTestMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/itwill3/dao/guest/3.guest_dao_jdbc.xml");
-		
+		GuestDao guestDao = (GuestDao)applicationContext.getBean("guestDao");
+		System.out.println("### "+guestDao.selectAll());
+		System.out.println("### "+guestDao.selectByNo(2));
 	}
 	
 }

446147f43cea67ffd507c1bca79c19a24e77e4d7
narae <iampnr91@gmail.com>
2021-09-13 09:55:59 +0900

    ..

--- springApp/src/main/java/com/itwill3/dao/SpringDataSourceMain.java
+++ springApp/src/main/java/com/itwill3/dao/SpringDataSourceMain.java
@@ -3,7 +3,10 @@ import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.itwill3.dao.user.User;
 
 public class SpringDataSourceMain {
 
@@ -34,9 +37,16 @@ public class SpringDataSourceMain {
 		String USER_SELECT_BY_ID = "select userid,password,name,email from userinfo where userid=?";
 		String USER_SELECT_ALL = "select userid,password,name,email from userinfo";
 		String USER_SELECT_BY_ID_COUNT = "select count(*) cnt from userinfo where userid=?";
-		System.out.println("---------------update[insert]--------------");
+		System.out.println("---------------JdbcTemplate.update[insert]--------------");
 		//int insertRowCount = jdbcTemplate.update(USER_INSERT,"aaaa","1111","에이에이","a@gmail.com");
 		//System.out.println("insert row count:"+insertRowCount);
+		
+		System.out.println("--- JdbcTemplate.queryForObject[select one]");
+		User user = jdbcTemplate.queryForObject(USER_SELECT_BY_ID, new Object[]{"aaaa"}, new BeanPropertyRowMapper<User>(User.class));
+		System.out.println(user);
+		System.out.println("--- JdbcTemplate.query[select many]");
+		
+		
 	}
 
 }

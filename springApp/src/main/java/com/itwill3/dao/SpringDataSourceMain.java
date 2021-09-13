83d1482e08c99c2fbd16db28d69032f5cfaf315b
narae <iampnr91@gmail.com>
2021-09-13 09:58:24 +0900

    ..

--- springApp/src/main/java/com/itwill3/dao/SpringDataSourceMain.java
+++ springApp/src/main/java/com/itwill3/dao/SpringDataSourceMain.java
@@ -32,21 +32,24 @@ public class SpringDataSourceMain {
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) applicationContext.getBean("jdbcTemplate");
 
 		String USER_INSERT = "insert into userinfo(userid,password,name,email) values(?,?,?,?)";
-		String USER_UPDATE = "update  userinfo set password=?,name=?,email=? where userid=?";
-		String USER_REMOVE = "delete userinfo where userid=?";
+		
 		String USER_SELECT_BY_ID = "select userid,password,name,email from userinfo where userid=?";
 		String USER_SELECT_ALL = "select userid,password,name,email from userinfo";
 		String USER_SELECT_BY_ID_COUNT = "select count(*) cnt from userinfo where userid=?";
+		
+		String USER_UPDATE = "update  userinfo set password=?,name=?,email=? where userid=?";
+		String USER_REMOVE = "delete userinfo where userid=?";
 		System.out.println("---------------JdbcTemplate.update[insert]--------------");
+		
 		//int insertRowCount = jdbcTemplate.update(USER_INSERT,"aaaa","1111","에이에이","a@gmail.com");
 		//System.out.println("insert row count:"+insertRowCount);
 		
-		System.out.println("--- JdbcTemplate.queryForObject[select one]");
-		User user = jdbcTemplate.queryForObject(USER_SELECT_BY_ID, new Object[]{"aaaa"}, new BeanPropertyRowMapper<User>(User.class));
+		System.out.println("---------------JdbcTemplate.queryForObject[select one]");
+		User user = jdbcTemplate.queryForObject(USER_SELECT_BY_ID, new Object[]{"aaaa"}, new BeanPropertyRowMapper<User>(User.class)); 
 		System.out.println(user);
-		System.out.println("--- JdbcTemplate.query[select many]");
 		
-		
+		System.out.println("---------------JdbcTemplate.query[select many]");
+	
 	}
 
 }

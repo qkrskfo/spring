0f6d49d1a1b0f289a3a0f18cd447a2e39806dc54
narae <iampnr91@gmail.com>
2021-09-13 09:48:45 +0900

    ..

--- springApp/src/main/java/com/itwill3/dao/SpringDataSourceMain.java
+++ springApp/src/main/java/com/itwill3/dao/SpringDataSourceMain.java
@@ -3,6 +3,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SpringDataSourceMain {
 
@@ -12,9 +13,10 @@ public class SpringDataSourceMain {
 		 *  - Spring Container객체생성
 		 */
 		System.out.println("------------Spring Container초기화시작----------------");
-		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/itwill3/dao/3.spring_jdbctemplate.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"com/itwill3/dao/3.spring_jdbctemplate.xml");
 		System.out.println("------------Spring Container초기화끝------------------");
-		
+		/*
 		DataSource apacheDataSource = (DataSource)applicationContext.getBean("apacheDataSource");
 		System.out.println("### 1.ApacheDataSource"+apacheDataSource);
 		System.out.println("### 1.ApacheDataSource Connection"+apacheDataSource.getConnection());
@@ -22,6 +24,19 @@ public class SpringDataSourceMain {
 		DataSource springDataSource = (DataSource)applicationContext.getBean("springDataSource");
 		System.out.println("### 2.SpringDataSource"+springDataSource);
 		System.out.println("### 2.SpringDataSource Connection"+springDataSource.getConnection());
+		*/
+		
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) applicationContext.getBean("jdbcTemplate");
+
+		String USER_INSERT = "insert into userinfo(userid,password,name,email) values(?,?,?,?)";
+		String USER_UPDATE = "update  userinfo set password=?,name=?,email=? where userid=?";
+		String USER_REMOVE = "delete userinfo where userid=?";
+		String USER_SELECT_BY_ID = "select userid,password,name,email from userinfo where userid=?";
+		String USER_SELECT_ALL = "select userid,password,name,email from userinfo";
+		String USER_SELECT_BY_ID_COUNT = "select count(*) cnt from userinfo where userid=?";
+		System.out.println("---------------update[insert]--------------");
+		//int insertRowCount = jdbcTemplate.update(USER_INSERT,"aaaa","1111","에이에이","a@gmail.com");
+		//System.out.println("insert row count:"+insertRowCount);
 	}
 
 }

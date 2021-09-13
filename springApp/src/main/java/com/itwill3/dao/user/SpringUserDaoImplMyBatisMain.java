32a95c3d0c4571a21460580f30c3429762946c23
narae <iampnr91@gmail.com>
2021-09-13 16:36:13 +0900

    ..

---
+++ springApp/src/main/java/com/itwill3/dao/user/SpringUserDaoImplMyBatisMain.java
@@ -0,0 +1,28 @@
+package com.itwill3.dao.user;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class SpringUserDaoImplMyBatisMain {
+
+	public static void main(String[] args)  throws Exception{
+		ApplicationContext applicationContext=
+				new ClassPathXmlApplicationContext("com/itwill3/dao/user/3.user_dao_springdao.xml");
+		UserDao userDao=(UserDao)applicationContext.getBean("userDao");
+		System.out.println("-------userDao.findUserList()------");
+		System.out.println(userDao.findUserList());
+		System.out.println("-------userDao.create()------");
+		System.out.println(userDao.create(new User("vvvv", "vvvv", "브이", "v@gmail.com")));
+		System.out.println("-------userDao.findUser()------");
+		System.out.println(userDao.findUser("vvvv"));
+		System.out.println("-------userDao.upadte()------");
+		System.out.println(userDao.update(new User("vvvv", "vvvv", "브이수정", "changev@gmail.com")));
+		System.out.println(userDao.findUser("vvvv"));
+		System.out.println("-------userDao.remove()------");
+		System.out.println(userDao.remove("vvvv"));
+		System.out.println(userDao.findUserList());
+
+	}
+
+}

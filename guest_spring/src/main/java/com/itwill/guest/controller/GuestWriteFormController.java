4909f5765cd078f0ec29089f3781824177df57b1
narae <iampnr91@gmail.com>
2021-09-14 15:40:22 +0900

    ..

--- guest_spring/src/main/java/com/itwill/guest/controller/GuestWriteFormController.java
+++ guest_spring/src/main/java/com/itwill/guest/controller/GuestWriteFormController.java
@@ -1,5 +1,19 @@
 package com.itwill.guest.controller;
 
-public class GuestWriteFormController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class GuestWriteFormController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//ModelAndView modelAndView = new ModelAndView();
+
+		String forwardPath="forward:/WEB-INF/views/guest_write_form.jsp";
+		return new ModelAndView(forwardPath);
+	}
 
 }

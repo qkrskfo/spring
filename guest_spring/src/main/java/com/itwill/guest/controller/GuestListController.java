585ca6c1832e0d176361fad8136b739a34badf23
narae <iampnr91@gmail.com>
2021-09-14 15:45:00 +0900

    ..

--- guest_spring/src/main/java/com/itwill/guest/controller/GuestListController.java
+++ guest_spring/src/main/java/com/itwill/guest/controller/GuestListController.java
@@ -1,5 +1,26 @@
 package com.itwill.guest.controller;
 
-public class GuestListController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.itwill.guest.GuestService;
+
+public class GuestListController implements Controller {
+
+	private GuestService guestService;
+	
+	public void setGuestService(GuestService guestService) {
+		this.guestService = guestService;
+	}
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String forwardPath="forward:/WEB-INF/views/guest_list.jsp";
+		guestService.selectAll();
+		return new ModelAndView(forwardPath);
+	}
 
 }

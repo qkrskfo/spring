ad98fd69cbeca5731c04d13b08cec48f404ed4d0
narae <iampnr91@gmail.com>
2021-09-15 18:15:39 +0900

    ..

--- guest_spring_annotation/src/main/java/com/itwill/guest/controller/GuestController.java
+++ guest_spring_annotation/src/main/java/com/itwill/guest/controller/GuestController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.itwill.guest.Guest;
 import com.itwill.guest.GuestService;
@@ -35,7 +36,7 @@ public class GuestController {
 		this.guestService = guestService;
 	}
 
-	//이름은 내 마음대로 하면 되는거야
+	
 	@RequestMapping("/guest_main.do")
 	public String guest_main() {
 		return "forward:/WEB-INF/views/guest_main.jsp";
@@ -59,9 +60,15 @@ public class GuestController {
 	@RequestMapping("/guest_view.do")
 	public String guest_view(HttpServletRequest request, HttpServletResponse response) {
 		String forwardPath ="";
+		String guest_noStr = request.getParameter("guest_no");
+		if(guest_noStr==null||guest_noStr.equals("")) {
+			return "redirect:guest_list.jsp";
+		}
 		try {
-			String quest_noStr = request.getParameter("guest_no");
-			Guest guest = guestService.selectByNo(Integer.parseInt(quest_noStr));
+			Guest guest = guestService.selectByNo(Integer.parseInt(guest_noStr));
+			if(guest==null) {
+				return "redirect:guest_list.do";
+			}
 			request.setAttribute("guest", guest);
 			forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
 		} catch (Exception e) {
@@ -76,7 +83,7 @@ public class GuestController {
 		return "forward:/WEB-INF/views/guest_write_form.jsp";
 	}
 	
-	@RequestMapping("/guest_write_action.do")
+	@RequestMapping(value="/guest_write_action.do", method = RequestMethod.POST)
 	public String guest_write_action(HttpServletRequest request, HttpServletResponse response) {
 		String forwardPath ="";
 		try {
@@ -87,8 +94,7 @@ public class GuestController {
 			String guest_content = request.getParameter("guest_content");
 			Guest guest = new Guest(0, guest_name, null, guest_email, guest_homepage, guest_title, guest_content);
 			guestService.insertGuest(guest);
-			//request.setAttribute("guest", guest);
-			forwardPath="redirect:guest_list.do";
+			forwardPath="redirect:guest_view.do?guest_no="+guest.getGuest_no();
 		} catch (Exception e) {
 			e.printStackTrace();
 			forwardPath="foward:/WEB-INF/views/guest_error.jsp";
@@ -96,12 +102,12 @@ public class GuestController {
 		return forwardPath;
 	}
 	
-	@RequestMapping("/guest_modify_form.do")
+	@RequestMapping(value="/guest_modify_form.do", method = RequestMethod.POST)
 	public String guest_modify_form(HttpServletRequest request, HttpServletRequest response) {
 		String forwardPath ="";
 		try {
-			String quest_noStr = request.getParameter("guest_no");
-			Guest guest = guestService.selectByNo(Integer.parseInt(quest_noStr));
+			String guest_noStr = request.getParameter("guest_no");
+			Guest guest = guestService.selectByNo(Integer.parseInt(guest_noStr));
 			request.setAttribute("guest", guest);
 			forwardPath="forward:/WEB-INF/views/guest_modify_form.jsp";
 		} catch (Exception e) {
@@ -111,7 +117,7 @@ public class GuestController {
 		return forwardPath;
 	}
 	
-	@RequestMapping("/guest_modify_action.do")
+	@RequestMapping(value="/guest_modify_action.do", method = RequestMethod.POST)
 	public String guest_modify_action(HttpServletRequest request, HttpServletResponse response) {
 		String forwardPath ="";
 		try {
@@ -126,7 +132,6 @@ public class GuestController {
 					null,guest_email,guest_homepage,
 					guest_title,guest_content);
 			guestService.updateGuest(updateGuest);
-			//request.setAttribute("guest", guest);
 			forwardPath="redirect:guest_view.do?guest_no="+guest_noStr;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -135,7 +140,7 @@ public class GuestController {
 		return forwardPath;
 	}
 	
-	@RequestMapping("/guest_remove_action.do")
+	@RequestMapping(value="/guest_remove_action.do", method = RequestMethod.POST)
 	public String guest_remove_action(HttpServletRequest request, HttpServletResponse response) {
 		String forwardPath ="";
 		try {

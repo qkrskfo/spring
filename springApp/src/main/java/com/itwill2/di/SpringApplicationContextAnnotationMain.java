568473f9748dcf59b7c2e1033ecdaa333ddcbfd6
narae <iampnr91@gmail.com>
2021-09-15 10:48:47 +0900

    ..

--- springApp/src/main/java/com/itwill2/di/SpringApplicationContextAnnotationMain.java
+++ springApp/src/main/java/com/itwill2/di/SpringApplicationContextAnnotationMain.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.itwill.user.User;
 import com.itwill.user.UserService;
+import com.itwill.user.annotation.UserAnnotation;
 
 public class SpringApplicationContextAnnotationMain {
 
@@ -15,14 +16,14 @@ public class SpringApplicationContextAnnotationMain {
 		System.out.println("------------Spring Container초기화시작----------------");
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/itwill2/di/2.bean_dependency_injection_annotation.xml");
 		System.out.println("------------Spring Container초기화끝------------------");
-		/*
+	
 		System.out.println("#### 1. Setter Injection(setter메쏘드를 통한 의존성주입)");
 		UserService userService=(UserService)applicationContext.getBean("userService");
 		userService.findUser(null);
 		userService.findUserList();
-		User setUser=(User)applicationContext.getBean("setUser");
+		UserAnnotation setUser=(UserAnnotation)applicationContext.getBean("setUser");
 		System.out.println("#### "+setUser);
-		
+		/*
 		System.out.println("#### 2. Constructor Injection(생성자를 통한 의존성주입)");
 		UserService conUserService=(UserService)applicationContext.getBean("conUserService");
 		conUserService.findUser(null);

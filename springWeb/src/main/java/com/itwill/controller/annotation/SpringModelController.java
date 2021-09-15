9619501f9685778dd5b28d3ab06dc51262170c05
narae <iampnr91@gmail.com>
2021-09-15 15:59:16 +0900

    ..

--- springWeb/src/main/java/com/itwill/controller/annotation/SpringModelController.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/SpringModelController.java
@@ -5,7 +5,10 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SpringModelController {
@@ -17,11 +20,51 @@ public class SpringModelController {
 		return "forward:/WEB-INF/views/spring_model.jsp";
 	}
 	
-	@RequestMapping("/model_map.do")
-	public String map(Map map) {
-		System.out.println("### map객체 --> "+map);
+	@RequestMapping("/model_model.do")
+	public String model(Model model) {
+		model.addAttribute("model", "모델 데이터");
+		/*
+		 * request.setAttribute("model", "모델데이터");
+		 */
 		return "forward:/WEB-INF/views/spring_model.jsp";
 	}
 	
+	@RequestMapping("/model_map.do")
+	public String map(Map map) {
+		System.out.println("### map객체 --> "+map);
+		map.put("map", "맵 데이터");
+		/*
+		 * request.setAttribute("map", "맵데이터");
+		 */
+		return "forward:/WEB-INF/views/spring_model.jsp";
+	}
 	
+	@RequestMapping("/model_modelmap.do")
+	public String modelmap(ModelMap modelMap) {
+		modelMap.addAttribute("modelmap", "모델맵 데이터");
+		/*
+		 * request.setAttribute("modelmap", "모델맵 데이터");
+		 */
+		return "forward:/WEB-INF/views/spring_model.jsp";
+	}
+	
+	@RequestMapping("/model_modelandview.do")
+	public ModelAndView modelandview() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("modelandview", "모델앤뷰 데이터");
+		/*
+		 * request.setAttribute("modelandview", "모델앤뷰 데이터");
+		 */
+		modelAndView.setViewName("forward:/WEB-INF/views/spring_model.jsp");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/model_all.do")
+	public String model_all(HttpServletRequest req,Model model,Map map,ModelMap modelMap) {
+		req.setAttribute("req", "리퀘스트 데이터");
+		model.addAttribute("model", "모델 데이터");
+		map.put("map", "맵 데이터");
+		modelMap.put("modelmap", "모델맵 데이터");
+		return "forward:/WEB-INF/views/spring_model.jsp";
+	}
 }

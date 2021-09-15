6f64437af423c0a265c2b417f6e8616dc68608fb
narae <iampnr91@gmail.com>
2021-09-15 17:09:06 +0900

    ..

--- guest_spring_annotation/src/main/java/com/itwill/guest/controller/GuestController.java
+++ guest_spring_annotation/src/main/java/com/itwill/guest/controller/GuestController.java
@@ -1,11 +1,20 @@
 package com.itwill.guest.controller;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itwill.guest.Guest;
+import com.itwill.guest.GuestService;
+
 @Controller
 public class GuestController {
-
+	
 	/*
 	<< 요청url (command) >>
 	/guest_main.do=com.itwill.guest.controller.GuestMainController
@@ -18,11 +27,35 @@ public class GuestController {
 	/guest_remove_action.do=com.itwill.guest.controller.GuestRemoveActionController
 	/guest_error.do=com.itwill.guest.controller.GuestErrorController
 	*/
+
+	private GuestService guestService;
 	
+	public GuestController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Autowired
+	public void setGuestService(GuestService guestService) {
+		this.guestService = guestService;
+	}
+
 	//이름은 내 마음대로 하면 되는거야
 	@RequestMapping("/guest_main.do")
 	public String guest_main() {
 		return "forward:/WEB-INF/views/guest_main.jsp";
 	}
 	
+	@RequestMapping("/guest_list.do")
+	public String guest_list(HttpServletRequest request, HttpServletResponse response) {
+		String forwardPath = "";
+		try {
+			ArrayList<Guest> guestList = guestService.selectAll();
+			request.setAttribute("guestList", guestList);
+			forwardPath = "forward:/WEB-INF/views/guest_list.jsp";
+		} catch (Exception e) {
+			e.printStackTrace();
+			forwardPath = "forward:/WEB-INF/views/guest_error.jsp";
+		}
+		return forwardPath;
+	}
 }

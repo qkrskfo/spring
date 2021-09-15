e1760bad3067f2d58e0e1f26ee88d82ba178334f
narae <iampnr91@gmail.com>
2021-09-15 14:36:48 +0900

    ..

--- springWeb/src/main/java/com/itwill/controller/annotation/HelloAnnotationController1.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/HelloAnnotationController1.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller(value = "helloController1")
+@Controller(value="helloController1")
 public class HelloAnnotationController1 {
 	public HelloAnnotationController1() {
 		System.out.println("### HelloAnnotationController1() 생성자");
 	}
 	
-	@RequestMapping(value = "/hello1.do")
+	@RequestMapping(value="/hello1.do")
 	public String hello1(HttpServletRequest request) {
 		System.out.println("### HelloAnnotationController1.hello1()호출");
 		request.setAttribute("msg", "안녕 HelloAnnotationController1");
--- springWeb/src/main/java/com/itwill/controller/annotation/HelloAnnotationMultiRequestMappingController.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/HelloAnnotationMultiRequestMappingController.java
@@ -6,26 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloAnnotationMultiRequestMappingController {
 	
-	@RequestMapping(value = "/hello3.do")
+	@RequestMapping(value="/hello3.do")
 	public String hello3() {
 		return "forward:/WEB-INF/views/hello3.jsp";
 	}
-	@RequestMapping(value = "/hello4.do")
+	@RequestMapping(value="/hello4.do")
 	public String hello4() {
 		return "forward:/WEB-INF/views/hello4.jsp";
 	}
-	@RequestMapping(value = "/hello5.do")
+	@RequestMapping(value="/hello5.do")
 	public String hello5() {
 		return "forward:/WEB-INF/views/hello5.jsp";
 	}
 	/*
 	 servlet[*.do]-->redirect[*.do]-->servlet[*.do]-->jsp
 	 */
-	@RequestMapping("/hello_redirect_servlet.do")
+	@RequestMapping(value="/hello_redirect_servlet.do")
 	public String hello_redirect_servlet() {
 		return "redirect:hello_redirected_servlet.do";
 	}
-	@RequestMapping("/hello_redirected_servlet.do")
+	@RequestMapping(value="/hello_redirected_servlet.do")
 	public String hello_redirected_servlet() {
 		return "forward:/WEB-INF/views/hello_redirected_servlet.jsp";
 	}

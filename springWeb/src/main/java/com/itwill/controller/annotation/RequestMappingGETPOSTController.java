5f63b7df6f81da9b1303d0ff458e5f9ddc812e87
narae <iampnr91@gmail.com>
2021-09-15 14:59:17 +0900

    ..

---
+++ springWeb/src/main/java/com/itwill/controller/annotation/RequestMappingGETPOSTController.java
@@ -0,0 +1,20 @@
+package com.itwill.controller.annotation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RequestMappingGETPOSTController {
+
+	@RequestMapping(value="/login.do", method = RequestMethod.GET)
+	public String login_form_get() {
+		return "forward:/WEB-INF/views/get_login_form.jsp";
+	}
+	
+	@RequestMapping(value="/login.do", method = RequestMethod.POST)
+	public String login_ation_post() {
+		return "forward:/WEB-INF/views/post_login_result.jsp";
+	}
+	
+}

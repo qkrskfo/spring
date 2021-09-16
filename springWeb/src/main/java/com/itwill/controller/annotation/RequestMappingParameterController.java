4bd2b3819f244e4e8862566350955f67587b5258
narae <iampnr91@gmail.com>
2021-09-16 10:27:37 +0900

    ..

--- springWeb/src/main/java/com/itwill/controller/annotation/RequestMappingParameterController.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/RequestMappingParameterController.java
@@ -16,13 +16,35 @@ public class RequestMappingParameterController {
 	}
 	
 	@RequestMapping(value="/parameter_guest_write_action.do", method = RequestMethod.POST)
-	public String parameter_guest_write_action(@RequestParam("guest_name") String guest_name, 
-			@RequestParam("guest_title") String guest_title, @RequestParam("guest_email") String guest_email,
-			@RequestParam String guest_homepage, @RequestParam String guest_content) {
+	public String model_attribute_guest_write_action() {
 		
-		Guest guest = new Guest(0, guest_name, null, guest_email, guest_homepage, guest_title, guest_content);
-		System.out.println("### guest:"+guest);
+		return "forward:/WEB-INF/views/guest_write_result.jsp";
+	}
+	
+	
+	
+	
+	@RequestMapping(value="/parameter_guest_write_action.do",method=RequestMethod.POST)
+	public String parameter_guest_write_action( @RequestParam("guest_name") String name,
+												@RequestParam("guest_title") String guest_title,
+												@RequestParam("guest_email") String guest_email,
+												@RequestParam String guest_homepage,
+												@RequestParam String guest_content) {
+		/*
+		 << @RequestParam >>
+			String guest_name = request.getParameter("guest_name");
+			String guest_email = request.getParameter("guest_email");
+			String guest_homepage = request.getParameter("guest_homepage");
+			String guest_title = request.getParameter("guest_title");
+			String guest_content = request.getParameter("guest_content");
+		 */
+		Guest guest=new Guest(0, name, null, guest_email, guest_homepage, guest_title, guest_content);
+		/*
+		 * GuestService메쏘드호출
+		 */
 		System.out.println("### @RequestParam ###");
+		System.out.println("### guest:"+guest);
 		return "forward:/WEB-INF/views/guest_write_result.jsp";
 	}
 }
+

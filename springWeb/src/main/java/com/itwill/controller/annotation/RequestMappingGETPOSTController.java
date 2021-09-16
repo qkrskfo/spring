1bc347704238e161f1bc108d4c4d0435dd3c302f
narae <iampnr91@gmail.com>
2021-09-16 09:38:52 +0900

    ..

--- springWeb/src/main/java/com/itwill/controller/annotation/RequestMappingGETPOSTController.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/RequestMappingGETPOSTController.java
@@ -1,8 +1,11 @@
 package com.itwill.controller.annotation;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RequestMappingGETPOSTController {
@@ -13,7 +16,20 @@ public class RequestMappingGETPOSTController {
 	}
 	
 	@RequestMapping(value="/login.do", method = RequestMethod.POST)
-	public String login_ation_post() {
+	public String login_ation_post(@RequestParam("id") String id, @RequestParam("password") String password, HttpSession session) {
+		/*
+		 * 로그인
+		 */
+		// requestParam을 쓰면 request 안써도 된데
+		
+		System.out.println("### id parameter : "+id);
+		System.out.println("### password parameter : "+password);
+		
+		boolean isLogin = true;
+		if(isLogin) {
+			session.setAttribute("sUserId", id);
+		}
+		
 		
 		return "forward:/WEB-INF/views/post_login_result.jsp";
 	}

67f689c88be501e759256836c59da960ab30d692
narae <iampnr91@gmail.com>
2021-09-16 13:43:07 +0900

    ..

--- springWeb/src/main/java/com/itwill/controller/annotation/ResponseController.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/ResponseController.java
@@ -1,6 +1,9 @@
 package com.itwill.controller.annotation;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.InternalResourceView;
@@ -24,7 +27,14 @@ public class ResponseController {
 	/**** xml 출력 view [XML view] ****/
 	
 	@RequestMapping("/response_xml_view_object.do")
-	public View response_xml_view_object() {
+	public View response_xml_view_object(Model model) {
+		ArrayList<String> friendList = new ArrayList<String>();
+		friendList.add("김수미");
+		friendList.add("김우미");
+		friendList.add("김미미");
+		friendList.add("김양미");
+		friendList.add("김가미");
+		model.addAttribute("friendList", friendList);
 		XMLView xmlView = new XMLView();
 		return xmlView;
 		
--- springWeb/src/main/java/com/itwill/controller/annotation/XMLView.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/XMLView.java
@@ -1,6 +1,7 @@
 package com.itwill.controller.annotation;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,10 +18,17 @@ public class XMLView extends AbstractView {
 
 	@Override
 	public void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("### DispatcherServlet이 View.renderMergedOutputModel() 호출한다.");
+		
+		ArrayList<String> friendList = (ArrayList<String>)model.get("friendList");
+		
 		response.setContentType("text/xml;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<?xml version='1.0' encoding='UTF-8' ?>");
 		out.println("<friends>");
+		for(String friend:friendList) {
+			out.println("<friend>"+friend+"</friend>");
+		}
 		out.println("</friends>");
 		
 	}

496902b91f8fd3a224b6856017cd6b29ff71a75c
narae <iampnr91@gmail.com>
2021-09-17 16:25:11 +0900

    ..

--- springWeb/src/main/java/com/itwill/controller/annotation/SpringCustomTagSPELController.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/SpringCustomTagSPELController.java
@@ -1,13 +1,16 @@
 package com.itwill.controller.annotation;
 
 import java.util.Date;
+import java.util.Locale;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.dto.Guest;
@@ -22,19 +25,44 @@ public class SpringCustomTagSPELController {
 	public void setMessageSource(MessageSource messageSource) {
 		this.messageSource = messageSource;
 	}
-
 	@RequestMapping("/jstl_fmt_i18n.do")
 	public String jstl_fmt_i18n() {
+		//return "forward:/WEB-INF/views/jstl_fmt_i18n.jsp";
 		return "jstl_fmt_i18n";
 	}
+	/************Spring JSP에서 국제화*******************/
+	
+	/************Spring Controller에서 국제화*******************
+	1. application-config.xml에 MessageSource빈설정
+	 <bean id="messageSource" class="org.springframework.context.support.ResourceBundleMessageSource">
+   		<property name="basenames" value="messages/guest,messages/messages,messages/user"/>
+   	 </bean>
+   	 
+   	 2. Controller객체 에서 MessageSource객체 injection
+   	 
+   	 3. Controller객체메쏘드에서 MessageSource객체 사용
+   	 
+   	 	String msg = messageSource.getMessage("user.existed.exception",
+											null,
+											requestLocale);
+   	 
+   *************************************************************/
 	
 	/*** SpringCustomTagSPELController 에서 국제화 해야함 ***/
+	/** Controller에서 국제화 **/
 	//버튼이름 취소/cancle, 등록/submit 같은거
-	@RequestMapping("/spring_customtag_i18n.do")
-	public String spring_customtag_i18n() {
+	@RequestMapping("/spring_controller_i18n.do")
+	public String spring_customtag_i18n(Locale requestLocale,Model model) {
+		//메시지를 붙여보내려면 인자에 model도 있어야해
 		
+				//Locale requestLocale = request.getLocale(); 인자에 Httpservletrequest 넣어야함
+		String msg = messageSource.getMessage("user.existed.exception",
+											null,
+											requestLocale);
+		model.addAttribute("user_existed_exception_controller_msg", msg);
 		return "spring_customtag_spel_i18n";
 	}
+	
 	
 	@RequestMapping("/spring_customtag_spel.do")
 	public String spring_customtag_spel(HttpServletRequest request) {
@@ -55,4 +83,5 @@ public class SpringCustomTagSPELController {
 							"오늘은목요일", "Spring EL에대해 공부합니다."));
 		return "spring_customtag_spel";
 	}
+	
 }
--- springWeb/src/main/webapp/WEB-INF/views/spring_customtag_spel_i18n.jsp
+++ springWeb/src/main/webapp/WEB-INF/views/spring_customtag_spel_i18n.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>    
+ 
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,27 +16,21 @@
     </bean>
 </xmp>
 <ol>
-	<li><s:message code="page.title"/></li>
-	<li><s:message code="button.login"/></li>
+	<li></li>
+	<li></li>
 	<!-- 
 		exception.userexisted={0} 사용자가 존재합니다..!!
 	 -->
-	<li><s:message code="exception.userexisted" arguments="guard"/></li>
-	<li><s:message code="exception.userexisted" arguments="xxxx"/></li> 
+	<li></li>
+	<li></li> 
 	<!-- 
 	nv.bc=홈 < {0} < {1}
 	nv.bc=HOME < {0} < {1}
 	 -->
-	 <li><s:message code="nv.bc" arguments="게스트,게스트 리스트" argumentSeparator=","/></li> 
-	 <s:eval expression="@messageSource.getMessage(
-	 		'nv.bc',
-	 		new Object[]{'게스트','게스트 리스트'},
-	 		new java.util.Locale('${pageContext.request.locale.language}','${pageContext.request.locale.country}'))"/>
-	 <li><s:message code="title.main"/></li>
-	 <li><s:eval expression="@messageSource.getMessage(
-	 		'title.main',
-	 		new Object[]{},
-	 		new java.util.Locale('${pageContext.request.locale.language}','${pageContext.request.locale.country}'))"/></li> 
+	 <li></li> 
+	 
+	 <li></li>
+	 <li></li> 
 	 <li>
 	 <li>Controller에서 생성한 메세지:${user_existed_exception_controller_msg}</li>
 	 <li>Service에서 생성한 메세지:${user_existed_exception_service_msg}</li>

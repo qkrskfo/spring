c69ec05b2e0fef548f22b4a35a954117be36d7b9
narae <iampnr91@gmail.com>
2021-09-17 17:09:12 +0900

    ..

--- user_spring/src/main/java/com/itwill/user/controller/UserController.java
+++ user_spring/src/main/java/com/itwill/user/controller/UserController.java
@@ -1,8 +1,29 @@
 package com.itwill.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+	/*
+	<<요청command>>
+	/user_main.do
+	/user_write_form.do
+	/user_write_action.do
+	/user_login_form.do
+	/user_login_action.do
+	/user_logout_action.do
+	/user_list.do
+	/user_view.do
+	/user_view_myinfo.do
+	/user_modify_form_myinfo.do
+	/user_modify_action_myinfo.do
+	/user_remove_action_myinfo.do
+	/user_error.do
+	 */
+	@RequestMapping("/user_main.do")
+	public String user_main() {
+		return "user_main";
+	}
 	
 }
--- user_spring/src/main/webapp/WEB-INF/views/include_common_top.jsp
+++ user_spring/src/main/webapp/WEB-INF/views/include_common_top.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <h1>
-	<a href="">회원관리 MODEL2 EL JSTL</a>
+	<a href="">회원관리 Spring</a>
 </h1>

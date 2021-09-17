a03fe8ad8b3ee705e4f7f442c2eed426ca6f531e
narae <iampnr91@gmail.com>
2021-09-17 11:54:56 +0900

    ..

---
+++ springWeb/src/main/java/com/itwill/controller/annotation/BusinessException1.java
@@ -0,0 +1,10 @@
+package com.itwill.controller.annotation;
+
+public class BusinessException1 extends Exception {
+	public BusinessException1(String msg) {
+		super(msg);
+	}
+	
+	
+	
+}
--- springWeb/src/main/java/com/itwill/controller/annotation/ExceptionControllerLocal.java
+++ springWeb/src/main/java/com/itwill/controller/annotation/ExceptionControllerLocal.java
@@ -1,17 +1,72 @@
 package com.itwill.controller.annotation;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class ExceptionControllerLocal {
-
+	
 	@RequestMapping("/business1.do")
-	public String business_method1() {
+	public String business_method1() throws BusinessException1 {
+		boolean b = true;
+		if(b) {
+			throw new BusinessException1("업무예외1 발생");
+		}
 		return "business_result1";
 	}
 	
 	@RequestMapping("/business2.do")
-	public String business_method2() {
+	public String business_method2() throws BusinessException2 {
+		boolean b = true;
+		if(b) {
+			throw new BusinessException2("업무예외2 발생");
+		}
 		return "business_result2";
+	}
+	
+	@RequestMapping("/business3.do")
+	public String business_method3() {
+		String name = null;
+		int length = name.length();
+		return "business_result3";
+	}
+	
+	/*######## @Exception Handler #######*/
+	@ExceptionHandler(Exception.class)
+	public String handle_runtime_exception(Exception e) {
+		return "global_error_result";
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	public String handle_runtime_exception(RuntimeException e) {
+		return "global_error_result";
+	}
+	
+	@ExceptionHandler(BusinessException1.class)
+	public String handle_business_exception1(BusinessException1 e) {
+		//return "forward:/WEB-INF/views/business_error_result.jsp";
+		return "business_error_result";
+	}
+	/*
+	@ExceptionHandler(BusinessException2.class)
+	public String handle_business_exception2(BusinessException2 e) {
+		//return "forward:/WEB-INF/views/business_error_result.jsp";
+		return "business_error_result";
+	}
+	*/
+	
+	@ExceptionHandler(BusinessException2.class)
+	public ModelAndView handle_business_exception2(BusinessException2 e) {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		//내가 인자를 받을 수 없을때는 modelandview를 리턴하게하면 됨
+		modelAndView.addObject("error_msg", e.getMessage());
+		modelAndView.setViewName("business_error_result");
+		//return "forward:/WEB-INF/views/business_error_result.jsp";
+		//return "business_error_result";
+		return modelAndView;
 	}
 	
 }

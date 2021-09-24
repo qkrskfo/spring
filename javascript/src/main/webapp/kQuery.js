4bd1716c3b4940064ca3ff5ea84ab21689b528f2
narae <iampnr91@gmail.com>
2021-09-24 10:27:12 +0900

    ..

--- javascript/src/main/webapp/kQuery.js
+++ javascript/src/main/webapp/kQuery.js
@@ -2,12 +2,13 @@
  * 
  */
 function hello(name, funcArg) {
-		document.write(">>> hello(name, funcArg) 실행<br>");
-		var msg = name+"님 안녕하세요!!";
-		funcArg(msg);
+	document.write(">>> hello(name, funcArg) 실행<br>");
+	var msg = name+"님 안녕하세요!!";
+	funcArg(msg);
 }
 
 function forEach(array, userCallbackFunction) {
+	document.write(">>> forEach(array, userCallbackFunction) 실행<br>");
 	for(var i =0; i<array.length; i++) {
 		userCallbackFunction(i, array[i]);
 	}

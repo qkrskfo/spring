56f8280742af63aa97a5613568a365abb2c1a583
narae <iampnr91@gmail.com>
2021-09-24 10:24:06 +0900

    ..

--- javascript/src/main/webapp/kQuery.js
+++ javascript/src/main/webapp/kQuery.js
@@ -1,13 +1,14 @@
 /**
  * 
  */
-function hello(fArg,name){
-	var msg = name+" 님 안녕하세요!!";
-	fArg(msg);
+function hello(name, funcArg) {
+		document.write(">>> hello(name, funcArg) 실행<br>");
+		var msg = name+"님 안녕하세요!!";
+		funcArg(msg);
 }
 
-function forEach(array,userCallbackFunc){
-	for(var i=0;i<array.length;i++){
-			userCallbackFunc(i,array[i]);	
+function forEach(array, userCallbackFunction) {
+	for(var i =0; i<array.length; i++) {
+		userCallbackFunction(i, array[i]);
 	}
 }

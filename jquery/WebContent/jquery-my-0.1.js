77f2cb56f74b6c8cb28769d38c5f561992170b59
narae <iampnr91@gmail.com>
2021-10-06 14:51:30 +0900

    ..

--- jquery/WebContent/jquery-my-0.1.js
+++ jquery/WebContent/jquery-my-0.1.js
@@ -13,7 +13,26 @@ window.jQuery=function(selector){
 			//이건 javascript object객체임(json객체)
 			//JSON new Object();
 			'elementNodeList':elementNodeList,
-			'elementNode':elementNode
+			'elementNode':elementNode,
+			'css':function(colorArg){
+				if(colorArg) {
+					for(var i=0; i<this.elementNodeList.length; i++){
+						this.elementNodeList[i].style.color=colorArg;
+					}
+				} else if(colorArg==undefined) {
+					return this.elementNodeList.style.color;
+				}	
+				return this;
+			},
+			'text':function(textArg){
+				for(var i=0; i<this.elementNodeList.length; i++) {
+					this.elementNodeList[i].firstChild.nodeValue=textArg;
+				}
+				return this;
+			}
+			//근데 02번에 만든거랑 이거랑 무슨 차이야..ㅠㅠ
+			//메소드를 만들려면 json에서 어떤식으로 만들어야하죠?
+			
 			
 		};
 		return jQueryWrapperObject;

af4b4f4cb4c89411dc497fbc1cf1af505163cf37
narae <iampnr91@gmail.com>
2021-10-07 14:01:35 +0900

    ..

--- jquery/WebContent/jquery-my-0.1.js
+++ jquery/WebContent/jquery-my-0.1.js
@@ -50,8 +50,7 @@ window.jQuery=function(selector){
 		
 	} else if(typeof selector=='object') {
 		/*
-		javascript의 표준객체
-		(dom객체, json, array, ...)
+		javascript 표준객체(Node,Document,Element,Text)
 			(document부터 dom tree에 있는 객체가 뭐라도 들어오면..)
 		*/
 	} else if(typeof selector=='function') {

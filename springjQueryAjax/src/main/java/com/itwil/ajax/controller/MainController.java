e2b8f3a3add8dacdcbb3c9f2338bf3663eadab35
narae <iampnr91@gmail.com>
2021-10-12 15:34:43 +0900

    ..

--- springjQueryAjax/src/main/java/com/itwil/ajax/controller/MainController.java
+++ springjQueryAjax/src/main/java/com/itwil/ajax/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	@RequestMapping(value="02.ajaxRequest.do")
 	public String ajaxRequest() {
-		return "";
+		return "forward:/WEB-INF/views/02.ajaxRequest.jsp";
 	}
 }

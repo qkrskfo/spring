700ff9f4f4115ac0b9c752d0354df16b42befc9d
narae <iampnr91@gmail.com>
2021-10-12 15:32:43 +0900

    ..

---
+++ springjQueryAjax/src/main/java/com/itwil/ajax/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.itwil.ajax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping(value="02.ajaxRequest.do")
+	public String ajaxRequest() {
+		return "";
+	}
+}

f62bce14396cad70b4add64e016adf8de7b4e62d
narae <iampnr91@gmail.com>
2021-10-13 15:43:37 +0900

    ..

--- guest_ajax_jquery_spring/src/main/java/com/itwill/guest/controller/GuestController.java
+++ guest_ajax_jquery_spring/src/main/java/com/itwill/guest/controller/GuestController.java
@@ -1,5 +1,12 @@
 package com.itwill.guest.controller;
 
-public class GuestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class GuestController {
+	@RequestMapping("/guest")
+	public String guest() {
+		return "guest";
+	}
 }

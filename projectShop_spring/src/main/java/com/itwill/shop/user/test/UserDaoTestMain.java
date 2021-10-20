f1c681345fb8d870c46d2dc0a8f202a7a7b1ab58
narae <iampnr91@gmail.com>
2021-10-20 11:52:16 +0900

    ..

---
+++ projectShop_spring/src/main/java/com/itwill/shop/user/test/UserDaoTestMain.java
@@ -0,0 +1,13 @@
+package com.itwill.shop.user.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserDaoTestMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/application-config.xml");
+
+	}
+
+}
